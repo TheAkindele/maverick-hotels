093dc00faa29253b31ab7ab8196298d949827447
Akindele Muhyideen <disgcfrguy@gmail.com>
2020-06-06 13:31:26 +0100

    initial commit for hotel app

---
+++ hotel/hotelData.js
@@ -0,0 +1,160 @@
+export const hotelRoomsData = [
+    {
+        id: 1,
+        roomType: 'Deluxe Room',
+        price: 25000,
+        guest: 2,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/5R8C8yc/room-12.jpg',
+        others: { subId: '1', bathroom: 'https://i.ibb.co/bvz4TWk/room-detail-16.jpg', settings: 'https://i.ibb.co/pWDvFyk/room-detail-17.jpg' }
+    },
+    {
+        id: 2,
+        roomType: 'deluxe room',
+        price: 30000,
+        guestLimit: 3,
+        breakfast: true,
+        roomImage: 'https://i.ibb.co/KjfYmn0/room-4.jpg',
+        others: { subId: '2', bathroom: 'https://i.ibb.co/x1MDR2J/room-detail-13.jpg', settings: 'https://i.ibb.co/8bYzWhL/room-detail-9.jpg' }
+    },
+    {
+        id: 3,
+        roomType: 'Executive standard',
+        price: 45000,
+        guestLimit: 2,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/4dQpCZG/room-16.jpg',
+        others: { subId: '3', bathroom: 'https://i.ibb.co/RzZmGLJ/bathroom.jpg', settings: 'https://i.ibb.co/mbjwWRK/room-detail-6.jpg' }
+    },
+    {
+        id: 4,
+        roomType: 'deluxe room',
+        price: 25000,
+        guestLimit: 2,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/ydxX1v6/room-7.jpg',
+        others: { subId: '4', bathroom: 'https://i.ibb.co/fGRMZ6K/room-detail-14.jpg', settings: 'https://i.ibb.co/cv4fhFm/room-detail-3.jpg' }
+    },
+    {
+        id: 5,
+        roomType: 'Executive standard',
+        price: 45000,
+        guestLimit: 4,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/S76vW63/room-3.jpg',
+        others: { subId: '5', bathroom: 'https://i.ibb.co/x1MDR2J/room-detail-13.jpg', settings: 'https://i.ibb.co/pWDvFyk/room-detail-17.jpg' }
+    },
+    {
+        id: 6,
+        roomType: 'executive standard',
+        price: 50000,
+        guestLimit: 2,
+        breakfast: true,
+        roomImage: 'https://i.ibb.co/MVLVZM2/room-10.jpg',
+        others: { subId: '6', bathroom: 'https://i.ibb.co/x1MDR2J/room-detail-13.jpg', settings: 'https://i.ibb.co/Wp3vj5F/room-detail-12.jpg' }
+    },
+    {
+        id: 7,
+        roomType: 'presidential palace',
+        price: 70000,
+        guestLimit: 2,
+        breakfast: true,
+        roomImage: 'https://i.ibb.co/DGBPbRQ/room-2.jpg',
+        others: { subId: '7', bathroom: 'https://i.ibb.co/RzZmGLJ/bathroom.jpg', settings: 'https://i.ibb.co/64W7tTR/room-detail-18.jpg' }
+    },
+    {
+        id: 8,
+        roomType: 'deluxe room',
+        price: 30000,
+        guestLimit: 3,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/dP7BxXQ/room-9.jpg',
+        others: { subId: '8', bathroom: 'https://i.ibb.co/fGRMZ6K/room-detail-14.jpg', settings: 'https://i.ibb.co/5xJ8XfF/room-detail-4.jpg' }
+    },
+    {
+        id: 9,
+        roomType: 'deluxe room',
+        price: 25000,
+        guestLimit: 2,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/S76vW63/room-3.jpg',
+        others: { subId: '9', bathroom: 'https://i.ibb.co/x1MDR2J/room-detail-13.jpg', settings: 'https://i.ibb.co/Wp3vj5F/room-detail-12.jpg' }
+    },
+    {
+        id: 10,
+        roomType: 'executive standard',
+        price: 50000,
+        guestLimit: 4,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/J5C5HK1/room-15.jpg',
+        others: { subId: '10', bathroom: 'https://i.ibb.co/RzZmGLJ/bathroom.jpg', settings: 'https://i.ibb.co/hmrwJqS/room-detail-11.jpg' }
+    },
+    {
+        id: 11,
+        roomType: 'deluxe room',
+        price: 25000,
+        guestLimit: 2,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/jWwR5r8/room-14.jpg',
+        others: { subId: '11', bathroom: 'https://i.ibb.co/bvz4TWk/room-detail-16.jpg', settings: 'https://i.ibb.co/pWDvFyk/room-detail-17.jpg' }
+    },
+    {
+        id: 12,
+        roomType: 'executive standard',
+        price: 50000,
+        guestLimit: 2,
+        breakfast: true,
+        roomImage: 'https://i.ibb.co/ft5qX8z/room-13.jpg',
+        others: { subId: '12', bathroom: 'https://i.ibb.co/x1MDR2J/room-detail-13.jpg', settings: 'https://i.ibb.co/4Y5BD5W/room-detail-8.jpg' }
+    },
+    {
+        id: 13,
+        roomType: 'presidential palace',
+        price: 80000,
+        guestLimit: 4,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/H7YpFHs/room-11.jpg',
+        others: { subId: '13', bathroom: 'https://i.ibb.co/Wsx3bh1/room-detail-20.jpg', settings: 'https://i.ibb.co/cYZtkJc/room-detail-22.jpg' }
+    },
+    {
+        id: 14,
+        roomType: 'deluxe room',
+        price: 35000,
+        guestLimit: 3,
+        breakfast: false,
+        roomImage: '',
+        others: { subId: '14', bathroom: 'https://i.ibb.co/mbjwWRK/room-detail-6.jpg', settings: 'https://i.ibb.co/T8Prh9s/room-detail-23.jpg' }
+    },
+    {
+        id: 15,
+        roomType: 'presidential palace',
+        price: 99999,
+        guestLimit: 4,
+        breakfast: true,
+        roomImage: 'https://i.ibb.co/y0D9QwQ/room-5.jpg',
+        others: { subId: '15', bathroom: 'https://i.ibb.co/87BHzDP/room-detail-10.jpg', settings: 'https://i.ibb.co/yXqXkpp/room-detail-19.jpg' }
+    },
+    {
+        id: 16,
+        roomType: 'executive standard',
+        price: 50000,
+        guestLimit: 3,
+        breakfast: false,
+        roomImage: 'https://i.ibb.co/fQKVT92/room-17.jpg',
+        others: { subId: '16', bathroom: 'https://i.ibb.co/pWDvFyk/room-detail-17.jpg', settings: 'https://i.ibb.co/56Hzm6Z/room-detail-1.jpg' }
+    }
+]
+
+export const homeCarousel = [
+    { id: 1, imageURL: 'https://i.ibb.co/Y7hVLmp/hotel-pool-1.jpg', name: 'Hotel front' },
+    { id: 2, imageURL: 'https://i.ibb.co/Bs6LwLJ/restaurant-1.jpg', name: 'Presidential restaurant' },
+    { id: 3, imageURL: 'https://i.ibb.co/m4GzsMD/beach-view.jpg', name: 'beach resort view' },
+    { id: 4, imageURL: 'https://i.ibb.co/Y0jxk8m/restaurant-2.jpg', name: 'executive restaurant' },
+    { id: 5, imageURL: 'https://i.ibb.co/WktD6Kf/hotel-pool-2.jpg', name: 'swimming pool' }
+]
+
+export const roomsCarousel = [
+    { id: 1, imageUrl: 'https://i.ibb.co/y0D9QwQ/room-5.jpg', roomType: 'presidential palace' },
+    { id: 2, imageUrl: 'https://i.ibb.co/S76vW63/room-3.jpg', roomType: 'excutive standard' },
+    { id: 3, imageUrl: 'https://i.ibb.co/5R8C8yc/room-12.jpg', roomType: 'deluxe room' },
+]
