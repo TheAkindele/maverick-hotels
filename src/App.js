3849a57d1efaca61f36f16352ac3b2cea5e952c4
TheAkindele <disgcfrguy@gmail.com>
2020-06-18 00:19:56 +0100

    made some modifications

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/navFolder/NavBar'
+import { Route, Switch } from 'react-router-dom'
+import Homepage from './components/homeFolder/Homepage';
+import RoomsPage from './components/roomsFolder/RoomsPage'
+import Room from './components/roomsFolder/Room'
 
 const App = () => {
   return (
-    <div className="App">
-
+    <div className="app">
+      <NavBar />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/rooms' component={RoomsPage} />
+        <Route path='/rooms/:roomId' component={Room} />
+      </Switch>
     </div>
   );
 }
