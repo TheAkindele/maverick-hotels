2b2e492475a58467a2009fd0ffad4ee0b462ad46
TheAkindele <akindelemuhyideen@gmail.com>
2020-07-22 15:44:22 +0100

    featured rooms page

---
+++ src/components/featured-folder/FeaturedRoom.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Container, Row, Col, Card, CardImg, CardBody, CardTitle } from 'reactstrap'
+import { FeaturedRoomsStyle, CardboxStyle } from './featuredRoomsStyle'
+import { Link } from 'react-router-dom'
+import { TitleStyle } from '../services-folder/servicesStyles'
+
+const FeaturedRooms = ({ featured }) => {
+    return (
+        <FeaturedRoomsStyle>
+            <TitleStyle>
+                <span>Featured Rooms</span>
+            </TitleStyle>
+            <CardboxStyle>
+                <Container>
+                    <Row>
+                        {
+                            featured.map(room => (
+                                <Col lg='4' xs='12' key={room.id} className='my-2'>
+                                    <Card >
+                                        <Link to={`/rooms`} className='to-rooms'>
+                                            <CardImg top width="100%" height="200px" src={room.imageUrl} alt="Card" />
+                                            <CardBody>
+                                                <CardTitle className='font-weight-bold'>{room.roomType.toUpperCase()}</CardTitle>
+                                            </CardBody>
+                                        </Link>
+                                    </Card>
+                                </Col>
+                            ))
+                        }
+                    </Row>
+                </Container>
+            </CardboxStyle>
+        </FeaturedRoomsStyle>
+    )
+}
+
+const mapStateToProps = state => ({
+    featured: state.hotelReducer.roomsCarousel
+})
+
+export default connect(mapStateToProps)(FeaturedRooms)
---
+++ src/components/featured-folder/featuredRoomsStyle.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const FeaturedRoomsStyle = styled.div`
+    background: white;
+    padding: 2vw 0;
+`
+
+export const CardboxStyle = styled.div`
+    padding: 3vw 0;
+`
