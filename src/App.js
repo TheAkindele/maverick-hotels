11758f86d0487b02aaf16c7b72b370ed76113c10
TheAkindele <akindelemuhyideen@gmail.com>
2020-07-24 07:28:42 +0100

    reorganized the router layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/navFolder/NavBar'
+import NavBar from './components/navFolder/Nav'
 import { Route, Switch } from 'react-router-dom'
 import Homepage from './components/homeFolder/Homepage';
-import RoomsPage from './components/roomsFolder/RoomsPage'
-import Room from './components/roomsFolder/Room'
+import RoomsPage from './components/roomPage-folder/RoomsPage'
+import Room from './components/room-folder/Room'
+import Contact from './components/Contact'
+import Footer from './components/footerFolder/Footer'
 
 const App = () => {
   return (
@@ -14,7 +16,9 @@ const App = () => {
         <Route exact path='/' component={Homepage} />
         <Route exact path='/rooms' component={RoomsPage} />
         <Route path='/rooms/:roomId' component={Room} />
+        <Route path='/contact' component={Contact} />
       </Switch>
+      <Footer />
     </div>
   );
 }
